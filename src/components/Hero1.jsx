69544ae153ec5a972f23083ac2a7c6f7e7b8a7f4
KenilGamer <127214403+kenilGamer@users.noreply.github.com>
2024-07-06 12:24:31 +0530

    gdbdgb

--- src/components/Hero1.jsx
+++ src/components/Hero1.jsx
@@ -5,7 +5,30 @@ import gsap from "gsap";
 
 function Hero1() {
   useGSAP(()=>{
-    gsap.from(".imgs1",{
+    const tl = gsap.timeline()
+    tl.from(".nav-l",{
+      duration:1,
+      y:-200
+    })
+    tl.from(".nav-r",{
+      duration:1,
+      y:-200,
+      stagger:0.3
+    })
+    tl.from(".text-1",{
+      duration:1,
+      x:-500,
+      stagger:0.3
+    })
+    tl.to(".btn",{
+      duration:1,
+      opacity:1
+    })
+    tl.from(".box-1",{
+      duration:1,
+      x:700,
+    })
+    tl.from(".imgs1",{
       duration:2,
       scale:0,
       // yoyo:true,
@@ -20,16 +43,16 @@ function Hero1() {
      <div className="shadow1 -z-10 w-96 h-96 top-0 -left-32 rounded-full absolute"></div>
      <div className="shadow1 -z-10 w-[70vh] h-[70vh] top-52 -right-8 rounded-full absolute"></div>
       <div className="hero-l w-1/2 max-md:w-full py-16 flex  max-md:items-center flex-col gap-16 justify-center">
-        <h1 className="text-7xl max-md:text-[10vw] max-md:text-center text-nowrap font-bold">Hi I’m Kenil</h1>
-        <p className="w-80 text-lg max-md:text-center">
+        <h1 className="text-7xl max-md:text-[10vw] max-md:text-center text-nowrap font-bold text-1">Hi I’m Kenil</h1>
+        <p className="w-80 text-lg max-md:text-center text-1">
           I'm a full-stack developer with 5 years of experience using React and
           NodeJS. Reach out if you'd like to learn more!
         </p>
-        <button className=" w-40 btn ">Contact Me</button>
+        <button className=" w-40 btn opacity-0 ">Contact Me</button>
       </div>
       <div className="hero-r flex items-center justify-center w-1/2 h-full max-md:w-[70vw] ">
-        <div className="w-[70vh] rounded-full h-[70vh] max-md:h-[35vh] bg-sky-800 flex items-center justify-center">
-        <img src="/public/Default_bg_remove_0_7e86b0ea-754c-4f86-bba0-d1426d0283df_0.png" className="object-cover w-full imgs1" alt="" />
+        <div className="w-[70vh] rounded-full box-1 h-[70vh] max-md:h-[35vh] bg-sky-800 flex items-center justify-center">
+        <img src="/public/Default_bg_remove_0_7e86b0ea-754c-4f86-bba0-d1426d0283df_0.png" className="object-cover w-full imgs1 " alt="" />
           
         </div>
       </div>
